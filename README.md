718ca3d8e479305932adcbc83a6da7adb67201b9
Ophir Yael <ofir.yael@gmail.com>
2020-11-02 16:04:00 +0200

    Add support for api request delay simulation

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ yarn start
 
 ```
 REACT_APP_API_BASE_URL=http://localhost:5000
+REACT_APP_API_DELAY=1000
 ```
 
 ### TODO
--- src/services/api-instance.ts
+++ src/services/api-instance.ts
@@ -1,3 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export const serverApi = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
+
+serverApi.interceptors.request.use(config => {
+  if (process.env.REACT_APP_API_DELAY) {
+    return delayApiRequest(config);
+  }
+  return config;
+});
+
+async function delayApiRequest(config: AxiosRequestConfig): Promise<AxiosRequestConfig> {
+  const delayMs = Number(process.env.REACT_APP_API_DELAY);
+  return new Promise(resolve => setTimeout(() => resolve(config), delayMs));
+}
